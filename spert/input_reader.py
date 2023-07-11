dff5c85481467b5cf034424586cf3cf088eb66e1
Quang Nguyen <nnquang122@gmail.com>
2023-07-11 10:40:25 +0100

    FIX input reader, produce sequence with length <=512 tokens only

--- spert/input_reader.py
+++ spert/input_reader.py
@@ -242,6 +242,8 @@ def _parse_tokens(jtokens, dataset, tokenizer):
         doc_tokens.append(token)
         doc_encoding += token_encoding
 
+    if len(doc_encoding) >= 512:
+        doc_encoding = doc_encoding[:511]
     doc_encoding += [tokenizer.convert_tokens_to_ids('[SEP]')]
 
     return doc_tokens, doc_encoding
